f4e308529d4ef4010e4d8c96ef79695545ec6a28
mani8054 <mmani658991@gmail.com>
2025-11-10 18:37:10 -0800

    modified the array

--- new.js
+++ new.js
@@ -1,19 +1,13 @@
-const input=require("readline-sync");
- let  data=array=[];
- console.log("array is empty");
-function show(){
-  if (data.length==0){
-    
-    for(i=0;i<=10;i++){
-      var neww=input.question("enter your name : ");
-      data.push(neww);
-      
-    }
-  }
-     console.log("rollno:"+i+"name of the students: "+data);
-    console.log("total students : "+data.length);
-    
-  }
- 
-show();
+const input = require("readline-sync");
+let data = [];
 
+for (let i = 0; i < 5; i++) {
+  let name = input.question("Enter student name " + (i + 1) + ": ");
+  data.push(name);
+}
+
+for (let i = 0; i < data.length; i++) {
+  console.log("Roll No " + (i + 1) + ": " + data[i]);
+}
+
+console.log("Total students: " + data.length);
